b8eb666336a79f0ba26a2384ac7892b26093292a
adhiiisetiawan <adhisetiawan518@gmail.com>
2020-08-23 20:16:41 +0700

    add comment in syntax

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import preprocessing, tfidf
 
 # Press the green button in the gutter to run the script.
@@ -17,6 +13,3 @@ if __name__ == '__main__':
 
     preprocessing_doc = preprocessing.text_preprocessing(document)
     print(tfidf.tfIdfCalculation([preprocessing_doc]))
-    # print(type(preprocessing_doc))
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- tfidf.py
+++ tfidf.py
@@ -2,8 +2,15 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 def tfIdfCalculation(dataset):
+    #mendeklarasikan object TfidfVectorizer
     tfIdfVectorizer = TfidfVectorizer(use_idf=True)
+
+    #melakukan perhitungan tf idf dengan text yang diambil dari argument yg bernama "dataset"
     tfIdf = tfIdfVectorizer.fit_transform(dataset)
+
+    #melakukan konversi hasil perhitungan tf idf ke dataframe
     df = pd.DataFrame(tfIdf[0].T.todense(), index=tfIdfVectorizer.get_feature_names(), columns=["TF-IDF"])
+
+    #sorting dataframe berdasarkan nilai tf-idf yang terbesar
     df = df.sort_values('TF-IDF', ascending=False)
     return df
