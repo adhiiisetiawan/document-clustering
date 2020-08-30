cdd581d7a2fb83ccbe87b750bc7bf17bc3c173ec
adhiiisetiawan <adhisetiawan518@gmail.com>
2020-08-30 15:48:00 +0700

    delete tf idf manual

--- Readme_TFIDF_Manual.txt
+++
@@ -1,3 +0,0 @@
-Di Kodingan TF IDF Manual 2
-
-jangan lupa ganti methodnya dari log() jadi log10()
--- tfidf_manual.py
+++ tfidf_manual.py
@@ -1,37 +1,156 @@
-import pandas as pd
-import numpy as np
+document = [
+    "Layang-layang terbang diangkasa",
+    "Burung- burung terbang diangkasa",
+    "Banyak layang-Layang berbentuk burung",
+    "Burung-burung di angkasa pulang di sore hari",
+    "Burung terbang untuk pulang ke sarang",
+]
+########################################################################################################################
+#buat list.append untuk bikin list yg isinya setiap kalimat yg setelah dipreprocessing
+preprocessing_doc = []
+import preprocessing
+for d in document:
+    preprocessing_doc.append(preprocessing.text_preprocessing(d))
+print("================================================================================================================")
+print("Ini hasil Preprocessing Setiap Dokumen2")
+print("================================================================================================================")
+print(preprocessing_doc)
+print("")
 
-def tfIdfCalculation(dataset):    
-    # Remove duplicates value
-    string = " "
-    data = string.join(dataset)
-    
-    tokens = list(dict.fromkeys(data.split()))
-    
-    doc_fr = []
-    for d in dataset:
-        tmp = []
-        for t in tokens:
-            tmp.append([t, d.split().count(t)])
-        doc_fr.append(tmp)
-    
-    c = 1
-    for tf in doc_fr:
-        print("doc", c)
-        c += 1
-        i = 0
-        for h in tf:
-            if(h[1] != 0):
-                tmp = list(h)
-                tmp[1] = np.log10(h[1])
-                tf[i] = tmp
-            print(tf[i][0], "\t\t= ", tf[i][1])
-            i += 1
-        print("=========================")
-    print(doc_fr)
-#    # Create the pandas DataFrame 
-#    df = pd.DataFrame(doc_fr, columns = ["doc 1", "doc 2", "doc 3"]) 
-#  
-#    # print dataframe. 
-#    print(df)
-    return
+########################################################################################################################
+#membuat daftar bag of words yang isinya kata2 displit setiap dokumen
+list_bow = []
+for d in range(len(preprocessing_doc)):
+    bow = preprocessing_doc[d].split(" ")
+    list_bow.append(bow)
+print("================================================================================================================")
+print("Ini Daftar Bag Of Words")
+print("================================================================================================================")
+print(list_bow)
+print(" ")
+
+
+########################################################################################################################
+#untuk membuat set kata (kata unik)
+preprocessing_doc = []
+for d in document:
+    preprocessing_doc.append(preprocessing.text_preprocessing(d))
+gabungan = " ".join(preprocessing_doc)
+kata2 = gabungan.split()
+print("================================================================================================================")
+print("Ini WordSet")
+print("================================================================================================================")
+wordset = set(kata2)
+print(wordset)
+print("")
+########################################################################################################################
+
+#membuat method pengitung kata2
+def word_count(str):
+    w = dict.fromkeys(wordset,0)
+    words = str.split()
+
+    for word in words:
+        if word in w:
+            w[word] += 1
+        else:
+            w[word] = 1
+
+    return w
+#append dictonary ke list
+list = []
+for kalimat in preprocessing_doc:
+    hasil = word_count(kalimat)
+    list.append(hasil)
+print("================================================================================================================")
+print("List of Dictonary")
+print("================================================================================================================")
+print(list)
+print(" ")
+########################################################################################################################
+#tampilan gambaran matriks hasil word count
+def dataFrame(hasil):
+    import pandas as pd
+    test = pd.DataFrame(hasil)
+    return test
+print("================================================================================================================")
+print("Tampilan Gambaran Matriks Hasil Word Count")
+print("================================================================================================================")
+print(dataFrame(list))
+print(" ")
+#######################################################################################################################
+#compute TF
+import math
+def computeTF(wordDict):
+    tfDict = {}
+    for word, count in wordDict.items():
+        #rumusnya masih count, math.log error entah kenapa
+        if count == 0:
+            tfDict[word] = 0
+        else:
+            tfDict[word] = 1 + math.log10(float(count))
+    return tfDict
+#eksekusi compute TF
+print("================================================================================================================")
+print("Hasil Eksekusi Compute TF (Masih ada kurang rumusnya TF Weighting)")
+print("================================================================================================================")
+for dicWord in list:
+    print(computeTF(dicWord))
+print(" ")
+########################################################################################################################
+#membuat compute IDF
+def computeIDF(docList):
+    import math
+    idfDict = {}
+    N = len(docList)
+    idfDict = dict.fromkeys(docList[0].keys(),0)
+    for doc in docList:
+        for word, val in doc.items():
+            if val > 0:
+                idfDict[word] +=1
+
+
+    for word, val in idfDict.items():
+        idfDict[word] = math.log10(N/float(val))
+
+
+    return idfDict
+print("================================================================================================================")
+print("Hasil Itung IDF")
+print("================================================================================================================")
+idfs = computeIDF(list)
+print(idfs)
+print(" ")
+
+########################################################################################################################
+#membuat compute TFIDF
+def computeTFIDF(tfBow,idfs):
+    tfidf = {}
+    for words, val in  tfBow.items():
+        tfidf[words] = val*idfs[words]
+
+    return tfidf
+list_tfidf = []
+#menghitung hasil tf dikali idfs
+for dicWord in list:
+    hasil_tfidf = computeTFIDF(dicWord,idfs)
+    list_tfidf.append(hasil_tfidf)
+print("================================================================================================================")
+print("List Hasil Itung TFIDF")
+print("================================================================================================================")
+print(list_tfidf)
+print(" ")
+
+
+import pandas as pd
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
+test2 = pd.DataFrame(list_tfidf)
+print("================================================================================================================")
+print("Ini Gambaran matriks hasil TFIDF")
+print("================================================================================================================")
+print("")
+print(test2)
+print("")
